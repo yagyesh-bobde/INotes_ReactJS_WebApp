698a47252c0407e4ce980174764475404868e5fc
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-04-19 17:24:49 +0530

    created validation for auth route using express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,24 @@
 const express = require('express')
-const router = express.Router()
 const User = require('../models/User')
 
-router.post('/', (req,res)=> {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+const router = express.Router()
+const { body, validationResult } = require('express-validator');
+
+router.post('/',[
+    body('name', 'Enter a Valid Name').isLength({ min: 3}),
+    body('email' , 'Enter a Valid mail-id').isEmail(),
+    body('password', 'Password must have atleast 5 characters').isLength({min:5})
+], (req,res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user)).catch(err => {console.log(err) 
+        res.json({error: 'Please enter a valid email'})});
 })
 
 module.exports = router
