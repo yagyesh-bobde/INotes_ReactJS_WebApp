1ab0127e8ed2d4e91aa84831dbc74b781fb07954
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-04-22 18:11:00 +0530

    used bcrypt for password encryption and jwtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
 
 const router = express.Router()
-const { body, validationResult } = require('express-validator');
-
+const JWT_SECRET = "Yagyeshis the best person in the world"
 router.post('/createuser',[
     body('name', 'Enter a Valid Name').isLength({ min: 3}),
     body('email' , 'Enter a Valid mail-id').isEmail(),
@@ -21,14 +24,26 @@ router.post('/createuser',[
         return res.status(400).json({error: 'This email is already registered with us.'})
     }
     else{
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt)
         // create a new user 
      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       })
+
+    const data = {
+        user: {
+            id:user.id
+        }
+    }
+
+    const authToken = jwt.sign(data, JWT_SECRET)
     //   res.json(user)
-    res.send(user)
+    // res.send(user)
+    res.json({authToken})
     }}
 
     catch(error) {
