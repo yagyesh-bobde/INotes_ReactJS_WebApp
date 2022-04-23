fa07c0d7d0c7cef10b2f6aa6c0bf2e0293d98244
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2022-04-23 14:06:04 +0530

    Created home navbar and about components and routes to those

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is INotebook</h1>
-    </div>
+  
+    <>
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />}/>
+    </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+ 
